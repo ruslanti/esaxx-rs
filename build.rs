8ed918d5bf158a5e467e0d6182da64af69970ef4
Ruslan Pislari <ruslan.pislari@gcore.com>
2023-12-07 18:33:48 +0200

    fix macos build for wasm32-wasi target

--- build.rs
+++ build.rs
@@ -2,8 +2,6 @@
 #[cfg(not(target_os = "macos"))]
 fn main() {
     cc::Build::new()
-        .cpp(true)
-        .flag("-std=c++11")
         .static_crt(true)
         .file("src/esaxx.cpp")
         .include("src")
@@ -14,9 +12,6 @@ fn main() {
 #[cfg(target_os = "macos")]
 fn main() {
     cc::Build::new()
-        .cpp(true)
-        .flag("-std=c++11")
-        .flag("-stdlib=libc++")
         .static_crt(true)
         .file("src/esaxx.cpp")
         .include("src")
